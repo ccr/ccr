7dfb112790a2abd189fa7dc3cb6378bd4575f99e
Edward Hartnett <edward.hartnett@noaa.gov>
2019-12-24 14:23:08 -0700

    starting to add HDF5 test

---
+++ include/ccr_test.h
@@ -0,0 +1,65 @@
+/**
+ * Header file for test code as part of the NetCDF Community Codec
+ * Repository.
+ *
+ * @author Ed Hartnett
+ * 12/24/19
+ */
+#ifndef _CCR_TEST_H
+#define _CCR_TEST_H
+
+#include <assert.h>
+
+/* Err is used to keep track of errors within each set of tests,
+ * total_err is the number of errors in the entire test program, which
+ * generally cosists of several sets of tests. */
+static int total_err = 0, err = 0;
+
+/* This macro prints an error message with line number and name of
+ * test program. */
+#define ERR do {							\
+        fflush(stdout); /* Make sure our stdout is synced with stderr. */ \
+        err++;                                                          \
+        fprintf(stderr, "Sorry! Unexpected result, %s, line: %d\n",	\
+                __FILE__, __LINE__);					\
+        fflush(stderr);							\
+        return 2;							\
+    } while (0)
+
+/* This macro prints an error message with line number and name of
+ * test program. */
+#define ERR2 do {							\
+        fflush(stdout); /* Make sure our stdout is synced with stderr. */ \
+        fprintf(stderr, "Sorry! Unexpected result, %s, line: %d\n",	\
+                __FILE__, __LINE__);					\
+        fflush(stderr);							\
+        return 2;							\
+    } while (0)
+
+/* After a set of tests, report the number of errors, and increment
+ * total_err. */
+#define SUMMARIZE_ERR do { \
+   if (err) \
+   { \
+      printf("%d failures\n", err); \
+      total_err += err; \
+      err = 0; \
+   } \
+   else \
+      printf("ok.\n"); \
+} while (0)
+
+/* This macro prints out our total number of errors, if any, and exits
+ * with a 0 if there are not, or a 2 if there were errors. Make will
+ * stop if a non-zero value is returned from a test program. */
+#define FINAL_RESULTS do { \
+   if (total_err) \
+   { \
+      printf("%d errors detected! Sorry!\n", total_err); \
+      return 2; \
+   } \
+   printf("*** Tests successful!\n"); \
+   return 0; \
+} while (0)
+
+#endif /* _CCR_TEST_H */
---
+++ test_h5/tst_h_bzip2.c
@@ -0,0 +1,116 @@
+/* This is part of the netCDF package. Copyright 2018 University
+   Corporation for Atmospheric Research/Unidata See COPYRIGHT file for
+   conditions of use.
+
+   Test HDF5 dataset code, even more. These are not intended to be
+   exhaustive tests, but they use HDF5 the same way that netCDF-4
+   does, so if these tests don't work, than netCDF-4 won't work
+   either.
+*/
+
+#include "ccr_test.h"
+#include <hdf5.h>
+#include <H5DSpublic.h>
+
+#define FILE_NAME "tst_h_bzip2.h5"
+#define STR_LEN 255
+
+int
+main()
+{
+   printf("\n*** Checking HDF5 variable functions some more.\n");
+   printf("*** Checking HDF5 variable compession and filters...");
+   {
+#define NUM_ELEMENTS 6
+#define MAX_NAME_LEN 50
+#define ELEMENTS_NAME "Elements"
+#define VAR_NAME "Sears_Zemansky_and_Young"
+#define NDIMS 2
+#define NX 60
+#define NY 120
+#define DEFLATE_LEVEL 3
+#define SIMPLE_VAR_NAME "data"
+
+      hid_t fapl_id, fcpl_id;
+      hid_t datasetid;
+      hid_t fileid, grpid, spaceid, plistid;
+      int data_in[NX][NY], data_out[NX][NY];
+      hsize_t fdims[NDIMS], fmaxdims[NDIMS];
+      hsize_t chunksize[NDIMS], dimsize[NDIMS], maxdimsize[NDIMS];
+      int x, y;
+
+      /* Create some data to write. */
+      for (x = 0; x < NX; x++)
+	 for (y = 0; y < NY; y++)
+	    data_out[x][y] = x * NY + y;
+
+      /* /\* Create file, setting latest_format in access propertly list */
+      /*  * and H5P_CRT_ORDER_TRACKED in the creation property list. *\/ */
+      /* if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR; */
+      /* if (H5Pset_libver_bounds(fapl_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST) < 0) ERR; */
+      /* if ((fcpl_id = H5Pcreate(H5P_FILE_CREATE)) < 0) ERR; */
+      /* if (H5Pset_link_creation_order(fcpl_id, H5P_CRT_ORDER_TRACKED|H5P_CRT_ORDER_INDEXED) < 0) ERR; */
+      /* if ((fileid = H5Fcreate(FILE_NAME, H5F_ACC_TRUNC, fcpl_id, fapl_id)) < 0) ERR; */
+
+      /* if ((grpid = H5Gopen(fileid, "/")) < 0) ERR; */
+
+      /* dimsize[0] = maxdimsize[0] = NX; */
+      /* dimsize[1] = maxdimsize[1] = NY; */
+      /* if ((spaceid = H5Screate_simple(NDIMS, dimsize, maxdimsize)) < 0) ERR; */
+
+      /* /\* Create property lust. *\/ */
+      /* if ((plistid = H5Pcreate(H5P_DATASET_CREATE)) < 0) ERR; */
+
+      /* /\* Set up chunksizes. *\/ */
+      /* chunksize[0] = NX; */
+      /* chunksize[1] = NY; */
+      /* if (H5Pset_chunk(plistid, NDIMS, chunksize) < 0)ERR; */
+
+      /* /\* Set up compression. *\/ */
+      /* if (H5Pset_deflate(plistid, DEFLATE_LEVEL) < 0) ERR; */
+
+      /* /\* Create the variable. *\/ */
+      /* if ((datasetid = H5Dcreate(grpid, SIMPLE_VAR_NAME, H5T_NATIVE_INT, */
+      /*   			 spaceid, plistid)) < 0) ERR; */
+
+      /* /\* Write the data. *\/ */
+      /* if (H5Dwrite(datasetid, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, */
+      /*   	   H5P_DEFAULT, data_out) < 0) ERR; */
+
+      /* if (H5Dclose(datasetid) < 0) ERR; */
+      /* if (H5Pclose(fapl_id) < 0 || */
+      /*     H5Fclose(fileid) < 0) */
+      /*    ERR; */
+      /* if (H5Pclose(fapl_id) < 0 || */
+      /*     H5Sclose(spaceid) < 0 || */
+      /*     H5Gclose(grpid) < 0 || */
+      /*     H5Fclose(fileid) < 0) */
+      /*    ERR; */
+
+      /* /\* Now reopen the file and check the order. *\/ */
+      /* if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR; */
+      /* if (H5Pset_libver_bounds(fapl_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST) < 0) ERR; */
+      /* if ((fileid = H5Fopen(FILE_NAME, H5F_ACC_RDONLY, fapl_id)) < 0) ERR; */
+      /* if ((grpid = H5Gopen(fileid, "/")) < 0) ERR; */
+
+      /* if ((datasetid = H5Dopen1(grpid, SIMPLE_VAR_NAME)) < 0) ERR; */
+      /* if ((spaceid = H5Dget_space(datasetid)) < 0) */
+      /* if (H5Sget_simple_extent_dims(spaceid, fdims, fmaxdims) > 0) ERR; */
+      /* if (H5Dread(datasetid, H5T_NATIVE_INT, H5S_ALL, */
+      /*   	  spaceid, H5P_DEFAULT, data_in) < 0) ERR; */
+
+      /* /\* Check the data. *\/ */
+      /* for (x = 0; x < NX; x++) */
+      /*    for (y = 0; y < NY; y++) */
+      /*       if (data_in[x][y] != data_out[x][y]) ERR_RET; */
+
+      /* if (H5Pclose(fapl_id) < 0 || */
+      /*     H5Dclose(datasetid) < 0 || */
+      /*     H5Sclose(spaceid) < 0 || */
+      /*     H5Gclose(grpid) < 0 || */
+      /*     H5Fclose(fileid) < 0) */
+      /*    ERR; */
+   }
+   SUMMARIZE_ERR;
+   FINAL_RESULTS;
+}
