b72730a3938025272d1ffff0d711a288004efaf0
Edward Hartnett <edward.hartnett@noaa.gov>
2019-12-30 10:04:38 -0700

    more test development

--- test/tst_bzip2.c
+++ test/tst_bzip2.c
@@ -43,13 +43,8 @@ main()
         int ncid;
         int dimid[NDIM2];
         int varid;
-        /* hid_t fapl_id, fcpl_id; */
-        /* hid_t datasetid; */
-        /* hid_t fileid, grpid, spaceid, plistid; */
-        int data_in[NX][NY], data_out[NX][NY];
-        /* hsize_t fdims[NDIMS], fmaxdims[NDIMS]; */
-        /* hsize_t chunksize[NDIMS], dimsize[NDIMS], maxdimsize[NDIMS]; */
-        /* const unsigned cd_values[1] = {9};          /\* bzip2 default level is 9 *\/ */
+        int data_out[NX][NY];
+        const unsigned int cd_values[1] = {9};          /* bzip2 default level is 9 */
         int x, y;
         const H5Z_class2_t H5Z_BZIP2[1] = {{
                 H5Z_CLASS_T_VERS,       /* H5Z_class_t version */
@@ -86,47 +81,39 @@ main()
         if (nc_def_dim(ncid, X_NAME, NX, &dimid[0]));
         if (nc_def_dim(ncid, Y_NAME, NY, &dimid[0]));
 
-        /* /\* Set up compression. *\/ */
         /* if (H5Pset_filter (plistid, (H5Z_filter_t)307, H5Z_FLAG_MANDATORY, */
         /*                    (size_t)1, cd_values) < 0) ERR; */
         /* /\* if (H5Pset_deflate(plistid, DEFLATE_LEVEL) < 0) ERR; *\/ */
 
         /* Create the variable. */
         if (nc_def_var(ncid, VAR_NAME, NC_INT, NDIM2, dimid, &varid)) ERR;
-        /* if ((datasetid = H5Dcreate2(grpid, SIMPLE_VAR_NAME, H5T_NATIVE_INT, */
-        /*                             spaceid, H5P_DEFAULT, plistid, H5P_DEFAULT)) < 0) ERR; */
 
-        /* /\* Write the data. *\/ */
-        /* if (H5Dwrite(datasetid, H5T_NATIVE_INT, H5S_ALL, H5S_ALL, */
-        /*   	   H5P_DEFAULT, data_out) < 0) ERR; */
+        /* Set up compression. */
+        if (nc_def_var_filter(ncid, varid, 307, 1, cd_values)) ERR;
 
+        /* Write the data. */
+        if (nc_put_var(ncid, varid, data_out)) ERR;
+
+        /* Close the file. */
         if (nc_close(ncid)) ERR;
 
-        /* Now reopen the file and check. */
-        if (nc_open(FILE_NAME, NC_NETCDF4, &ncid)) ERR;
-        if (nc_close(ncid)) ERR;
-        /* if ((fapl_id = H5Pcreate(H5P_FILE_ACCESS)) < 0) ERR; */
-        /* if (H5Pset_libver_bounds(fapl_id, H5F_LIBVER_LATEST, H5F_LIBVER_LATEST) < 0) ERR; */
-        /* if ((fileid = H5Fopen(FILE_NAME, H5F_ACC_RDONLY, fapl_id)) < 0) ERR; */
-        /* if ((grpid = H5Gopen2(fileid, "/", H5P_DEFAULT)) < 0) ERR; */
+        {
+            int data_in[NX][NY];
 
-        /* if ((datasetid = H5Dopen1(grpid, SIMPLE_VAR_NAME)) < 0) ERR; */
-        /* if ((spaceid = H5Dget_space(datasetid)) < 0) */
-        /* if (H5Sget_simple_extent_dims(spaceid, fdims, fmaxdims) > 0) ERR; */
-        /* if (H5Dread(datasetid, H5T_NATIVE_INT, H5S_ALL, */
-        /*   	  spaceid, H5P_DEFAULT, data_in) < 0) ERR; */
+            /* Now reopen the file and check. */
+            if (nc_open(FILE_NAME, NC_NETCDF4, &ncid)) ERR;
 
-        /* /\* Check the data. *\/ */
-        /* for (x = 0; x < NX; x++) */
-        /*    for (y = 0; y < NY; y++) */
-        /*       if (data_in[x][y] != data_out[x][y]) ERR; */
+            /* Read the data. */
+            if (nc_get_var(ncid, varid, data_in)) ERR;
 
-        /* if (H5Pclose(fapl_id) < 0 || */
-        /*     H5Dclose(datasetid) < 0 || */
-        /*     H5Sclose(spaceid) < 0 || */
-        /*     H5Gclose(grpid) < 0 || */
-        /*     H5Fclose(fileid) < 0) */
-        /*    ERR; */
+            /* Check the data. */
+            for (x = 0; x < NX; x++)
+               for (y = 0; y < NY; y++)
+                  if (data_in[x][y] != data_out[x][y]) ERR;
+
+            /* Close the file. */
+            if (nc_close(ncid)) ERR;
+        }
     }
     SUMMARIZE_ERR;
     FINAL_RESULTS;
