1dfe399a50777fd85a3fdf2004739ded245fa152
Edward Hartnett <edward.hartnett@noaa.gov>
2019-12-30 14:27:47 -0700

    further development of LZ4

--- src/ccr.c
+++ src/ccr.c
@@ -13,6 +13,10 @@
 size_t H5Z_filter_bzip2(unsigned int flags, size_t cd_nelmts,
                         const unsigned int cd_values[], size_t nbytes,
                         size_t *buf_size, void **buf);
+
+size_t H5Z_filter_lz4(unsigned int flags, size_t cd_nelmts,
+                      const unsigned int cd_values[], size_t nbytes,
+                      size_t *buf_size, void **buf);
 /**
  * Initialize Community Codec Repository library.
  *
@@ -32,12 +36,26 @@ nc_initialize_ccr()
             NULL,                       /* The "set local" callback     */
             (H5Z_func_t)H5Z_filter_bzip2,         /* The actual filter function   */
         }};
+    const H5Z_class2_t H5Z_LZ4[1] = {{
+            H5Z_CLASS_T_VERS,       /* H5Z_class_t version */
+            (H5Z_filter_t)LZ4_ID,         /* Filter id number             */
+            1,              /* encoder_present flag (set to true) */
+            1,              /* decoder_present flag (set to true) */
+            "lz4",                  /* Filter name for debugging    */
+            NULL,                       /* The "can apply" callback     */
+            NULL,                       /* The "set local" callback     */
+            (H5Z_func_t)H5Z_filter_lz4,         /* The actual filter function   */
+        }};
+
     /* char plugin_path[MAX_LEN + 1]; */
     /* if (H5PLget(0, plugin_path, MAX_LEN) < 0) ERR; */
     /* printf("plugin_path %s\n", plugin_path); */
 
     if (H5Zregister(H5Z_BZIP2) < 0)
-        return 2;
+        return NC_EFILTER;
+
+    if (H5Zregister(H5Z_LZ4) < 0)
+        return NC_EFILTER;
 
     return 0;
 }
