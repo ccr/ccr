c409c2a4d9539e24801013254ef31252c9630b9c
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-11-18 12:43:41 -0700

    now running tests with HDF5_PLUGIN_PATH

---
+++ test/run_tests.sh
@@ -0,0 +1,9 @@
+# This script runs the tests in the CCR project.
+#
+# Ed Hartnett 11/18/20
+
+# Set the plugin path to find plugin.
+export HDF5_PLUGIN_PATH=../src/.libs
+
+# Run the example.
+./tst_bzip2
