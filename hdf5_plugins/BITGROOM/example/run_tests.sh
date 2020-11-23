c6635a871bdcd096d5c9ba9136dcd2d46a0ac922
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-11-23 12:19:59 -0700

    fixed bitgroom example test run

---
+++ hdf5_plugins/BITGROOM/example/run_tests.sh
@@ -0,0 +1,9 @@
+# This script runs the BITGROOM examples in the CCR project.
+#
+# Ed Hartnett 11/23/20
+
+# Set the plugin path to find plugin.
+export HDF5_PLUGIN_PATH=../src/.libs
+
+# Run the example.
+./h5ex_d_bitgroom
