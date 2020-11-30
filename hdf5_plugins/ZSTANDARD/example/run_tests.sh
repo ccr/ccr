6010a7a157e98df08525f03258571d49f1927169
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-11-30 11:51:04 -0700

    fixed zstandard example

---
+++ hdf5_plugins/ZSTANDARD/example/run_tests.sh
@@ -0,0 +1,9 @@
+# This script runs the ZSTANDARD examples in the CCR project.
+#
+# Ed Hartnett 11/30/20
+
+# Set the plugin path to find plugin.
+export HDF5_PLUGIN_PATH=../src/.libs
+
+# Run the example.
+./h5ex_d_zstandard
