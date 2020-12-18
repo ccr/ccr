478a9b8faae2a2f0fb764f3848dcf596a499aad8
Edward Hartnett <Edward.Hartnett@noaa.gov>
2020-12-18 07:00:29 -0700

    removed LZ4 functions

--- src/ccr.c
+++ src/ccr.c
@@ -31,23 +31,6 @@
  * - nf90_def_var_bzip2()
  * - nf90_inq_var_bzip2()
  *
- * LZ4
- *
- * "LZ4 is a lossless data compression algorithm that is focused on
- * compression and decompression speed. It belongs to the LZ77 family
- * of byte-oriented compression schemes" (Wikipedia). For full
- * documentaton see http://lz4.github.io/lz4/. For additional info see
- * https://github.com/lz4/lz4 and
- * https://en.wikipedia.org/wiki/LZ4_(compression_algorithm).
- *
- * In C:
- * - nc_def_var_lz4()
- * - nc_inq_var_lz4()
- *
- * In Fortran:
- * - nf90_def_var_lz4()
- * - nf90_inq_var_lz4()
- *
  * BitGroom
  *
  * The BitGroom filter quantizes the mantissa of floating point values
@@ -190,96 +173,96 @@ nc_inq_var_bzip2(int ncid, int varid, int *bzip2p, int *levelp)
     return 0;
 }
 
-/**
- * Turn on lz4 compression for a variable.
- *
- * @param ncid File ID.
- * @param varid Variable ID.
- * @param level From 1 to 9. Set the block size to 100k, 200k ... 900k
- * when compressing. (lz4 default level is 9).
- *
- * @return 0 for success, error code otherwise.
- * @author Ed Hartnett
- */
-int
-nc_def_var_lz4(int ncid, int varid, int level)
-{
-    unsigned int cd_value = level;
-    int ret;
+/* /\** */
+/*  * Turn on lz4 compression for a variable. */
+/*  * */
+/*  * @param ncid File ID. */
+/*  * @param varid Variable ID. */
+/*  * @param level From 1 to 9. Set the block size to 100k, 200k ... 900k */
+/*  * when compressing. (lz4 default level is 9). */
+/*  * */
+/*  * @return 0 for success, error code otherwise. */
+/*  * @author Ed Hartnett */
+/*  *\/ */
+/* int */
+/* nc_def_var_lz4(int ncid, int varid, int level) */
+/* { */
+/*     unsigned int cd_value = level; */
+/*     int ret; */
 
-    /* Level must be between 1 and 9. */
-    if (level < 1 || level > 9)
-        return NC_EINVAL;
+/*     /\* Level must be between 1 and 9. *\/ */
+/*     if (level < 1 || level > 9) */
+/*         return NC_EINVAL; */
 
-    if (!H5Zfilter_avail(LZ4_ID))
-    {
-        printf ("lz4 filter not available.\n");
-        return NC_EFILTER;
-    }
+/*     if (!H5Zfilter_avail(LZ4_ID)) */
+/*     { */
+/*         printf ("lz4 filter not available.\n"); */
+/*         return NC_EFILTER; */
+/*     } */
 
-    /* Set up the lz4 filter for this var. */
-    if ((ret = nc_def_var_filter(ncid, varid, LZ4_ID, 1, &cd_value)))
-        return ret;
+/*     /\* Set up the lz4 filter for this var. *\/ */
+/*     if ((ret = nc_def_var_filter(ncid, varid, LZ4_ID, 1, &cd_value))) */
+/*         return ret; */
 
-    return 0;
-}
+/*     return 0; */
+/* } */
 
-/**
- * Learn whether lz4 compression is on for a variable, and, if so,
- * the level setting.
- *
- * @param ncid File ID.
- * @param varid Variable ID.
- * @param lz4p Pointer that gets a 0 if lz4 is not in use for this
- * var, and a 1 if it is. Ignored if NULL.
- * @param levelp Pointer that gets the level setting (from 1 to 9), if
- * bzlip2 is in use. Ignored if NULL.
- *
- * @return 0 for success, error code otherwise.
- * @author Ed Hartnett
- */
-int
-nc_inq_var_lz4(int ncid, int varid, int *lz4p, int *levelp)
-{
-    unsigned int level;
-    unsigned int id;
-    size_t nparams;
-    int lz4 = 0; /* Is lz4 in use? */
-    int ret;
+/* /\** */
+/*  * Learn whether lz4 compression is on for a variable, and, if so, */
+/*  * the level setting. */
+/*  * */
+/*  * @param ncid File ID. */
+/*  * @param varid Variable ID. */
+/*  * @param lz4p Pointer that gets a 0 if lz4 is not in use for this */
+/*  * var, and a 1 if it is. Ignored if NULL. */
+/*  * @param levelp Pointer that gets the level setting (from 1 to 9), if */
+/*  * bzlip2 is in use. Ignored if NULL. */
+/*  * */
+/*  * @return 0 for success, error code otherwise. */
+/*  * @author Ed Hartnett */
+/*  *\/ */
+/* int */
+/* nc_inq_var_lz4(int ncid, int varid, int *lz4p, int *levelp) */
+/* { */
+/*     unsigned int level; */
+/*     unsigned int id; */
+/*     size_t nparams; */
+/*     int lz4 = 0; /\* Is lz4 in use? *\/ */
+/*     int ret; */
 
-    /* Get filter information. */
-    ret = nc_inq_var_filter(ncid, varid, &id, &nparams, &level);
-    if (ret == NC_ENOFILTER)
-    {
-	if (lz4p)
-	    *lz4p = 0;
-	return 0;
-    }
-    else if (ret)
-	return ret;
+/*     /\* Get filter information. *\/ */
+/*     ret = nc_inq_var_filter(ncid, varid, &id, &nparams, &level); */
+/*     if (ret == NC_ENOFILTER) */
+/*     { */
+/* 	if (lz4p) */
+/* 	    *lz4p = 0; */
+/* 	return 0; */
+/*     } */
+/*     else if (ret) */
+/* 	return ret; */
 
-    /* Is lz4 in use? */
-    if (id == LZ4_ID)
-        lz4++;
+/*     /\* Is lz4 in use? *\/ */
+/*     if (id == LZ4_ID) */
+/*         lz4++; */
 
-    /* Does caller want to know if lz4 is in use? */
-    if (lz4p)
-        *lz4p = lz4;
+/*     /\* Does caller want to know if lz4 is in use? *\/ */
+/*     if (lz4p) */
+/*         *lz4p = lz4; */
 
-    /* If lz4 is in use, check parameter. */
-    if (lz4)
-    {
-        /* For lz4, there is one parameter. */
-        if (nparams != 1)
-            return NC_EFILTER;
+/*     /\* If lz4 is in use, check parameter. *\/ */
+/*     if (lz4) */
+/*     { */
+/*         /\* For lz4, there is one parameter. *\/ */
+/*         if (nparams != 1) */
+/*             return NC_EFILTER; */
 
-        /* Tell the caller, if they want to know. */
-        if (levelp)
-            *levelp = level;
-    }
+/*         /\* Tell the caller, if they want to know. *\/ */
+/*         if (levelp) */
+/*             *levelp = level; */
+/*     } */
 
-    return 0;
-}
+/*     return 0; */
+/* } */
 
 /**
  * Turn on BitGroom quantization for a variable.
